e0c22019b87da98f418084935aa5b06e421a2ce5
Umutesi Sonia <r0879042@student.thomasmore.be>
2025-05-20 23:42:59 +0200

    Integrated Stripe API

--- presso-api/routes/api.php
+++ presso-api/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CapsuleController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\PaymentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,3 +36,5 @@ Route::delete('/cart/{id}', [CartController::class, 'destroy']); // remove item
 // ABOUT CHECKOUT
 Route::get('/checkout', [CheckoutController::class, 'index']);
 Route::post('/checkout', [CheckoutController::class, 'store']);
+
+Route::post('/checkout', [PaymentController::class, 'checkout']);
