22f4799bc6597bf867fa391b37dd3ad984d93508
Umutesi Sonia <162119361+r0879042@users.noreply.github.com>
2025-05-29 20:44:47 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,19 +19,19 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $capsules = [
-            ['name' => 'Vienna Lungo', 'type' => 'Original', 'image' => 'vienna-lungo.jpg', 'description' => 'Inspired by Vienna’s coffee tradition.', 'flavour_profile' => 'Spicy & Woody'],
-            ['name' => 'Stockholm Lungo', 'type' => 'Original', 'image' => 'stockholm-lungo.jpg', 'description' => 'Swedish balance & elegance.', 'flavour_profile' => 'Balanced & Smooth'],
-            ['name' => 'Cape Town Lungo', 'type' => 'Original', 'image' => 'capetown-lungo.jpg', 'description' => 'Full-bodied African roast.', 'flavour_profile' => 'Bold & Intense'],
-            ['name' => 'Shanghai Lungo', 'type' => 'Original', 'image' => 'shanghai-lungo.jpg', 'description' => 'Chinese refined roast.', 'flavour_profile' => 'Smooth & Delicate'],
-            ['name' => 'Tokyo Lungo', 'type' => 'Original', 'image' => 'tokyo-lungo.jpg', 'description' => 'Elegant Japanese roast.', 'flavour_profile' => 'Floral & Delicate'],
-            ['name' => 'Paris Espresso', 'type' => 'Original', 'image' => 'paris-espresso.jpg', 'description' => 'French inspired roast.', 'flavour_profile' => 'Rich & Smooth'],
+            ['name' => 'Vienna Lungo', 'type' => 'Original', 'image' => 'vienna-lungo.jpg', 'description' => 'Inspired by Vienna’s coffee tradition.', 'flavour_profile' => 'Spicy & Woody', 'price' => 4.80],
+            ['name' => 'Stockholm Lungo', 'type' => 'Original', 'image' => 'stockholm-lungo.jpg', 'description' => 'Swedish balance & elegance.', 'flavour_profile' => 'Balanced & Smooth', 'price' => 5.30],
+            ['name' => 'Cape Town Lungo', 'type' => 'Original', 'image' => 'capetown-lungo.jpg', 'description' => 'Full-bodied African roast.', 'flavour_profile' => 'Bold & Intense', 'price' => 4.80],
+            ['name' => 'Shanghai Lungo', 'type' => 'Original', 'image' => 'shanghai-lungo.jpg', 'description' => 'Chinese refined roast.', 'flavour_profile' => 'Smooth & Delicate', 'price' => 5.30],
+            ['name' => 'Tokyo Lungo', 'type' => 'Original', 'image' => 'tokyo-lungo.jpg', 'description' => 'Elegant Japanese roast.', 'flavour_profile' => 'Floral & Delicate', 'price' => 5.30],
+            ['name' => 'Paris Espresso', 'type' => 'Original', 'image' => 'paris-espresso.jpg', 'description' => 'French inspired roast.', 'flavour_profile' => 'Rich & Smooth', 'price' => 5.80],
 
-            ['name' => 'Vienna Lungo', 'type' => 'Vertuo', 'image' => 'vienna-lungo.jpg', 'description' => 'Inspired by Vienna’s coffee tradition.', 'flavour_profile' => 'Spicy & Woody'],
-            ['name' => 'Stockholm Lungo', 'type' => 'Vertuo', 'image' => 'stockholm-lungo.jpg', 'description' => 'Swedish balance & elegance.', 'flavour_profile' => 'Balanced & Smooth'],
-            ['name' => 'Cape Town Lungo', 'type' => 'Vertuo', 'image' => 'capetown-lungo.jpg', 'description' => 'Full-bodied African roast.', 'flavour_profile' => 'Bold & Intense'],
-            ['name' => 'Shanghai Lungo', 'type' => 'Vertuo', 'image' => 'shanghai-lungo.jpg', 'description' => 'Chinese refined roast.', 'flavour_profile' => 'Smooth & Delicate'],
-            ['name' => 'Tokyo Lungo', 'type' => 'Vertuo', 'image' => 'tokyo-lungo.jpg', 'description' => 'Elegant Japanese roast.', 'flavour_profile' => 'Floral & Delicate'],
-            ['name' => 'Paris Espresso', 'type' => 'Vertuo', 'image' => 'paris-espresso.jpg', 'description' => 'French inspired roast.', 'flavour_profile' => 'Rich & Smooth'],
+            ['name' => 'Vienna Lungo', 'type' => 'Vertuo', 'image' => 'vienna-lungo.jpg', 'description' => 'Inspired by Vienna’s coffee tradition.', 'flavour_profile' => 'Spicy & Woody', 'price' => 7.80],
+            ['name' => 'Stockholm Lungo', 'type' => 'Vertuo', 'image' => 'stockholm-lungo.jpg', 'description' => 'Swedish balance & elegance.', 'flavour_profile' => 'Balanced & Smooth', 'price' => 5.60],
+            ['name' => 'Cape Town Lungo', 'type' => 'Vertuo', 'image' => 'capetown-lungo.jpg', 'description' => 'Full-bodied African roast.', 'flavour_profile' => 'Bold & Intense', 'price' => 6.80],
+            ['name' => 'Shanghai Lungo', 'type' => 'Vertuo', 'image' => 'shanghai-lungo.jpg', 'description' => 'Chinese refined roast.', 'flavour_profile' => 'Smooth & Delicate', 'price' => 5.60],
+            ['name' => 'Tokyo Lungo', 'type' => 'Vertuo', 'image' => 'tokyo-lungo.jpg', 'description' => 'Elegant Japanese roast.', 'flavour_profile' => 'Floral & Delicate', 'price' => 7.80],
+            ['name' => 'Paris Espresso', 'type' => 'Vertuo', 'image' => 'paris-espresso.jpg', 'description' => 'French inspired roast.', 'flavour_profile' => 'Rich & Smooth', 'price' => 6.80],
         ];
 
         foreach ($capsules as $capsule) {
