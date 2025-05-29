1a43ff24b6c7e0410e2f903910f441d84e81891e
Olga Ingelaere <113905169+OlyaIngelaere@users.noreply.github.com>
2025-05-29 11:22:15 +0200

    Add loginpage

---
+++ app/Http/Controllers/Web/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::guard('web')->attempt($credentials, $request->filled('remember'))) {
+            return redirect()->intended('/welcome');
+        }
+
+        return back()->withErrors([
+            'email' => 'Invalid credentials.',
+        ])->withInput($request->except('password'));
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+}
